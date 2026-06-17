5f997cd9631873ec91729109034849064a2c9307
AT-render <andreatortolo@gmail.com>
2026-06-17 08:45:34 +0200

    Add files via upload

--- interazioni/script.js
+++ interazioni/script.js
@@ -1,6 +1,10 @@
 // Import dei moduli locali
-import * as THREE from '../js/three.module.js';
-import { GLTFLoader } from '../js/GLTFLoader.js';
+// import * as THREE from '../js/three.module.js';
+// import { GLTFLoader } from '../js/GLTFLoader.js';
+
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.164/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.164/examples/jsm/loaders/GLTFLoader.js';
+
 
 // Inizializzazione scena, camera e renderer
 const scene = new THREE.Scene();
