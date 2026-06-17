956f5c4de4205bb84b034a298197bd2e62d49f8a
AT-render <andreatortolo@gmail.com>
2026-06-17 08:39:09 +0200

    Add files via upload

--- interazioni/script.js
+++ interazioni/script.js
@@ -1,6 +1,5 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.164/build/three.module.js';
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.164/examples/jsm/loaders/GLTFLoader.js';
-
+import * as THREE from '../js/three.module.js';
+import { GLTFLoader } from '../js/GLTFLoader.js';
 
 let scene, camera, renderer, modello;
 let legsMat, topMat;
